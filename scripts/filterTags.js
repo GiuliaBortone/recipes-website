8716e9b7b47f1e9be4a1a35e939ec347e1199c23
GiuliaBortone <giulia.bortone@mail.polimi.it>
2024-02-27 13:24:55 +0100

    filter when click on filter buttons

--- scripts/filterTags.js
+++ scripts/filterTags.js
@@ -1,3 +1,19 @@
+import {getAllRecipes, showRecipes} from "./createRecipeContainers.js";
+
 export function filterFromTag(tag) {
     console.log("hello " + tag)
+    keepRecipesWithFilter(tag)
+}
+
+function keepRecipesWithFilter(tagName) {
+    const parentRecipeContainer = document.querySelector('.parent-container')
+    parentRecipeContainer.innerHTML = ""
+
+    const allRecipes = getAllRecipes()
+    if (tagName === "All recipes") {
+        showRecipes(parentRecipeContainer, allRecipes)
+    }
+
+    const filteredRecipes = allRecipes.filter(recipe => recipe.tags.includes(tagName))
+    showRecipes(parentRecipeContainer, filteredRecipes)
 }
